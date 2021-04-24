7f344f0b3b83734bf7271b57256707512eb9f96a
LPaube <lpaube@hotmail.com>
2021-04-24 16:14:14 -0400

    changed folders

--- libft/libft.h
+++ libft/libft.h
@@ -6,10 +6,31 @@
 /*   By: laube <louis-philippe.aube@hotmail.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 14:18:43 by laube             #+#    #+#             */
-/*   Updated: 2021/04/17 14:18:44 by laube            ###   ########.fr       */
+/*   Updated: 2021/04/23 14:46:49 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+void	*ft_memset(void *b, int c, size_t len);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+int	ft_strnequ(char const *s1, char const *s2, size_t n);
+int	ft_strlen(const char *s);
+int	ft_strequ(char const *s1, char const *s2);
+void	ft_strdel(char **as);
+void	ft_strclr(char *s);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putstr(char const *s);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr(int n);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putendl(char const *s);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putchar(char c);
+void	ft_memdel(void **ap);
+void	*ft_memalloc(size_t size);
+void	ft_bzero(void *s, size_t n);
+
+/*	GENERATED PROTOTYPES (PROTOTYPICAL) 	*/
+char	*ft_strnew(size_t size);
+
