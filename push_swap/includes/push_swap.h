9c6daaa0fe9bc87c49e6cf36f4b7d51848363890
laube <laube@laubes-MacBook-Pro.local>
2021-06-10 22:44:06 -0400

    Makefile works?

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,12 +6,15 @@
 /*   By: laube <louis-philippe.aube@hotma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 17:58:22 by laube             #+#    #+#             */
-/*   Updated: 2021/06/10 14:52:00 by laube            ###   ########.fr       */
+/*   Updated: 2021/06/10 22:19:58 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/include/ft_printf.h"
-#include "../libft/libft/libft.h"
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/include/ft_printf.h"
+# include "../libft/libft/libft.h"
 
 typedef struct s_part
 {
@@ -19,24 +22,24 @@ typedef struct s_part
 	int				midp;
 	int				sorted;
 	struct s_part	*next;
-} t_part;
+}	t_part;
 
 typedef struct s_stack
 {
 	int				num;
 	struct s_part	*part_head;
 	struct s_stack	*next;
-} t_stack;
+}	t_stack;
 
 t_part	*create_node_part(void);
-int	partitioning_b(t_stack *head1, t_stack *head2);
-int	partitioning_a(t_stack *head1, t_stack *head2);
-int	part_midp(t_part *part, t_stack *head, int head_type);
+int		partitioning_b(t_stack *head1, t_stack *head2);
+int		partitioning_a(t_stack *head1, t_stack *head2);
+int		part_midp(t_part *part, t_stack *head, int head_type);
 void	solve_3(t_stack *head1);
 void	solve_5(t_stack *head1, t_stack *head2);
-int	partitioning_a(t_stack *head1, t_stack *head2);
+int		partitioning_a(t_stack *head1, t_stack *head2);
 void	part_control_a(t_stack *head1, t_stack *head2);
-int	partitioning_b(t_stack *head1, t_stack *head2);
+int		partitioning_b(t_stack *head1, t_stack *head2);
 void	part_control_b(t_stack *head1, t_stack *head2);
 char	*ft_push_a(t_stack *head1, t_stack *head2);
 char	*ft_push_b(t_stack *head1, t_stack *head2);
@@ -50,21 +53,23 @@ t_part	*create_node_part(void);
 t_stack	*ft_create_node(void);
 void	init_part1(t_stack *head1);
 t_stack	*ft_build_stack(char **argv);
-int	*stack_to_arr(t_stack *head, int len);
-int	arr_quicksort(int *arr, int low, int high);
+int		*stack_to_arr(t_stack *head, int len);
+int		arr_quicksort(int *arr, int low, int high);
 void	arr_quicksort_control(int *arr, int low, int high);
-int	arr_is_sorted(int *arr, int len, int head_type);
+int		arr_is_sorted(int *arr, int len, int head_type);
 void	free_part(t_stack *head);
-int	not_in_part(t_stack *head, int head_type, int midp);
-int	part_midp(t_part *part, t_stack *head, int head_type);
+int		not_in_part(t_stack *head, int head_type, int midp);
+int		part_midp(t_part *part, t_stack *head, int head_type);
 void	rev_rotation(t_stack *head, int rot_count, int head_type);
-int	stack_len(t_stack *head);
-int	ft_check_order(t_stack *head1, t_stack *head2);
-int	last_num(t_stack *head);
-int	has_unsorted(t_stack *head);
-int	low_num(t_stack *head);
+int		stack_len(t_stack *head);
+int		ft_check_order(t_stack *head1, t_stack *head2);
+int		last_num(t_stack *head);
+int		has_unsorted(t_stack *head);
+int		low_num(t_stack *head);
 void	amt_is_2(t_stack *head1, t_part *tmp1);
-int	sub_a(t_stack *head1, t_stack *head2, t_part *curr_part, int *rot_count);
+int		sub_a(t_stack *h1, t_stack *h2, t_part *curr_part, int *rot_count);
 void	ft_print_stacks(t_stack *head1, t_stack *head2);
-int	high_num(t_stack *head);
-int	check_errors(int argc, char **argv);
+int		high_num(t_stack *head);
+int		check_errors(int argc, char **argv);
+
+#endif
--- push_swap/srcs/partition_a2.c
+++ push_swap/srcs/partition_a2.c
@@ -6,28 +6,28 @@
 /*   By: laube <louis-philippe.aube@hotmail.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 20:46:54 by laube             #+#    #+#             */
-/*   Updated: 2021/06/09 20:47:58 by laube            ###   ########.fr       */
+/*   Updated: 2021/06/10 22:19:52 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	sub_a(t_stack *head1, t_stack *head2, t_part *curr_part, int *rot_count)
+int	sub_a(t_stack *h1, t_stack *h2, t_part *curr_part, int *rot_count)
 {
 	int	counter;
 
 	counter = 0;
-	if (not_in_part(head1, 1, curr_part->midp))
+	if (not_in_part(h1, 1, curr_part->midp))
 		return (0);
-	if (head1->next->num < curr_part->midp)
+	if (h1->next->num < curr_part->midp)
 	{
-		ft_printf("%s", ft_push_b(head1, head2));
+		ft_printf("%s", ft_push_b(h1, h2));
 		counter++;
 		curr_part->amt--;
 	}
 	else
 	{
-		ft_printf("%s", ft_rotate_a(head1));
+		ft_printf("%s", ft_rotate_a(h1));
 		(*rot_count)++;
 	}
 	return (counter);
