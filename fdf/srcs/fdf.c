1399641eef1a021c8b49e18be124a8170fe3061c
LPaube <lpaube@hotmail.com>
2021-06-12 23:18:09 -0400

    Testing mlx lib and X11

--- fdf/srcs/fdf.c
+++ fdf/srcs/fdf.c
@@ -6,8 +6,50 @@
 /*   By: laube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 12:14:48 by laube             #+#    #+#             */
-/*   Updated: 2021/06/12 12:14:57 by laube            ###   ########.fr       */
+/*   Updated: 2021/06/12 22:39:53 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <mlx.h>
+#include <X11/Xlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <X11/extensions/XShm.h>
 
+
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello!!!!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	my_mlx_pixel_put(&img, 5, 6, 0x00FF0000);
+	my_mlx_pixel_put(&img, 5, 7, 0x00FF0000);
+	my_mlx_pixel_put(&img, 5, 8, 0x00FF0000);
+	my_mlx_pixel_put(&img, 5, 20, 0x00FF0000);
+	my_mlx_pixel_put(&img, 5, 22, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
