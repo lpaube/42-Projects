7c54c82009d10c4373fb804c3ab69debb48162bf
LPaube <lpaube@hotmail.com>
2021-06-18 21:16:03 -0400

    almost printing linesf

--- fdf/includes/fdf.h
+++ fdf/includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: laube <louis-philippe.aube@hotmail.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 16:29:35 by laube             #+#    #+#             */
-/*   Updated: 2021/06/15 19:02:03 by laube            ###   ########.fr       */
+/*   Updated: 2021/06/18 21:04:05 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,34 @@
 # include "../libft/libft.h"
 # include "../libft/get_next_line.h"
 
-typedef struct	s_fdf
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-}	t_fdf;
+# define HEIGHT	1080
+# define WIDTH	1920
+# define ABS(a)	(a) < 0 ? -(a) : (a)
 
 typedef struct	s_map
 {
 	int	width;
 	int	height;
 }	t_map;
+
+typedef struct	s_fdf
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	t_map	*map;
+}	t_fdf;
+
+typedef struct	s_point
+{
+	int	x;
+	int	y;
+	int	z;
+	int	color;
+}	t_point;
 
 #endif
--- fdf/srcs/fdf.c
+++ fdf/srcs/fdf.c
@@ -6,7 +6,7 @@
 /*   By: laube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 12:14:48 by laube             #+#    #+#             */
-/*   Updated: 2021/06/16 19:14:18 by laube            ###   ########.fr       */
+/*   Updated: 2021/06/18 21:15:06 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,15 @@ int	esc_hook(int keycode, t_fdf *fdf)
 }
 
 
-t_fdf	*fdf_init(void)
+t_fdf	*fdf_init(t_map *map)
 {
 	t_fdf	*fdf;
 
+	fdf = malloc(sizeof(t_fdf));
 	fdf->mlx_ptr = mlx_init();
 	fdf->win_ptr = mlx_new_window(fdf->mlx_ptr, 1000, 800, "Cool window!");
+	fdf->img_ptr = mlx_new_image(fdf->mlx_ptr, WIDTH, HEIGHT);
+	fdf->map = map;
 	return (fdf);
 }
 
@@ -50,13 +53,72 @@ t_map	*map_init(void)
 	return (map);
 }
 
+void	color_to_pixel(t_fdf *fdf, int x, int y, int color)
+{
+	int	pxl_pos;
+
+	pxl_pos = (y * fdf->line_length) + (x * (fdf->bits_per_pixel / 8));
+
+	if (fdf->endian == 1)
+	{
+		fdf->addr[pxl_pos + 0] = (color >> 24);
+		fdf->addr[pxl_pos + 1] = (color >> 16) & 0xFF;
+		fdf->addr[pxl_pos + 2] = (color >> 8) & 0xFF;
+		fdf->addr[pxl_pos + 3] = (color) & 0xFF;
+	}
+	else if (fdf->endian == 0)
+	{
+		fdf->addr[pxl_pos + 0] = (color) & 0xFF;
+		fdf->addr[pxl_pos + 1] = (color >> 8) & 0xFF;
+		fdf->addr[pxl_pos + 2] = (color >> 16) & 0xFF;
+		fdf->addr[pxl_pos + 3] = (color >> 24);
+	}
+}
+
+int	get_pxl_pos(t_fdf *fdf, int x, int y)
+{
+	int	pxl_pos;
+
+	pxl_pos = (y * fdf->line_length) + (x * (fdf->bits_per_pixel / 8));
+	return (pxl_pos);
+}
+
+int	draw_line(t_fdf *fdf, t_point p1, t_point p2)
+{
+	double	delta_x;
+	double	delta_y;
+
+	delta_x = ABS(p2.x - p1.x);
+	delta_y = ABS(p2.y - p1.y);
+}
+
+t_point get_point(int x, int y, int z, int color)
+{
+	t_point	point;
+
+	point.x = x;
+	point.y = y;
+	point.z = z;
+	point.color = color;
+	return (point);
+}
+
+void	draw_control(t_fdf *fdf)
+{
+	fdf->addr = mlx_get_data_addr(fdf->img_ptr, &fdf->bits_per_pixel, &fdf->line_length, &fdf->endian);
+	draw_line(fdf, get_point(0, 0, 0, 0x0000FFFF), get_point(300, 300, 0, 0x0000FFFF));
+	mlx_put_image_to_window(fdf->mlx_ptr, fdf->win_ptr, fdf->img_ptr, 0, 0);
+}
+
 int main(void)
 {
 	t_fdf	*fdf;
 	t_map	*map;
 
 	map = map_init();
-	fdf = fdf_init();
+	fdf = fdf_init(map);
+//	events_control(fdf);
+	draw_control(fdf);
 	mlx_key_hook(fdf->win_ptr, esc_hook, fdf);
 	mlx_loop(fdf->mlx_ptr);
 }
