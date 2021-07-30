e4e1af3beeca8e40c6367d0e580e603f5331bdd3
laube <laube@laubes-MacBook-Pro.local>
2021-07-30 16:36:29 -0300

    fixed threads

--- philo/incls/philo.h
+++ philo/incls/philo.h
@@ -6,29 +6,32 @@
 /*   By: laube <louis-philippe.aube@hotmail.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 18:55:24 by laube             #+#    #+#             */
-/*   Updated: 2021/07/29 00:12:12 by laube            ###   ########.fr       */
+/*   Updated: 2021/07/30 16:36:13 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef PHILO_H
 # define PHILO_H
 
-typedef struct	s_philos
-{
-	char	state; // e: eating | t: thinking | s: sleeping
-	int		holds_left;
-	int		holds_right;
-}				t_philos;
-
 typedef struct	s_configs
 {
-	int	phil_num;
+	int	phils_num;
+	int	forks_num;
 	int	die_time;
 	int	eat_time;
 	int	sleep_time;
 	int	eat_num;
 	int	eat_num_active;
-	t_philos	*philos; 
+	int	*forks; // 0: available | 1: in use
 }				t_configs;
+
+typedef struct	s_philos
+{
+	int			id;
+	char		state; // e: eating | t: thinking | s: sleeping | d: dead
+	int			holds_left;
+	int			holds_right;
+	t_configs	*configs;
+}				t_philos;
 
 # endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: laube <louis-philippe.aube@hotmail.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 11:07:05 by laube             #+#    #+#             */
-/*   Updated: 2021/07/29 00:37:38 by laube            ###   ########.fr       */
+/*   Updated: 2021/07/30 16:33:04 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,17 @@ int	ft_atoi(const char *str)
 	return (curr_num * state);
 }
 
-t_philos	*init_philos(int phil_num)
-{
-	t_philos	*philos;
-	int			i;
 
-	philos = malloc(sizeof(t_philos) * phil_num);
+int	*init_forks(int	num)
+{
+	int	i;
+	int	*forks;
+
+	forks = malloc(sizeof(int) * num);
 	i = 0;
-	while (i < phil_num)
-	{
-		philos[i].state = 't';
-		philos[i].holds_left = 0;
-		philos[i].holds_right = 0;
-		i++;
-	}
-	return (philos);
+	while (i < num)
+		forks[i++] = 0;
+	return (forks);
 }
 
 t_configs	*init_configs(int argc, char **argv)
@@ -62,13 +58,14 @@ t_configs	*init_configs(int argc, char **argv)
 
 	i = 0;
 	configs = malloc(sizeof(t_configs));
-	configs->phil_num = ft_atoi(argv[1]);
+	configs->phils_num = ft_atoi(argv[1]);
+	configs->forks_num = configs->phils_num + 1;
 	configs->die_time = ft_atoi(argv[2]);
 	configs->eat_time = ft_atoi(argv[3]);
 	configs->sleep_time = ft_atoi(argv[4]);
 	configs->eat_num = 0;
 	configs->eat_num_active = 0;
-	configs->philos = init_philos(ft_atoi(argv[1]));
+	configs->forks = init_forks(configs->phils_num + 1);
 	if (argc == 6)
 	{
 		configs->eat_num = ft_atoi(argv[5]);
@@ -77,24 +74,49 @@ t_configs	*init_configs(int argc, char **argv)
 	return (configs);
 }
 
-void	*routine(void *nothing)
+void	*init_philos(int argc, char **argv)
 {
-	printf("threadID: %p\n", pthread_self());
+	t_philos	*philos;
+	t_configs	*configs;
+	int			i;
+
+	configs = init_configs(argc, argv);
+	philos = malloc(sizeof(t_philos) * configs->phils_num);
+	i = 0;
+	while (i < configs->phils_num)
+	{
+		philos[i].state = 't';
+		philos[i].id = i;
+		philos[i].holds_left = 0;
+		philos[i].holds_right = 0;
+		philos[i].configs = configs;
+		i++;
+	}
+	return (philos);
+}
+
+void	*routine(void *philo)
+{
+	printf("id of philo: %d\n", ((struct s_philos *)philo)->id);
 	return (NULL);
 }
 
-int	init_threads(t_configs *configs)
+int	init_threads(t_philos *philos)
 {
-	int	i;
-	pthread_t tid;
+	pthread_t	*tid;
+	int			i;
 
+	tid = malloc(sizeof(tid) * philos->configs->phils_num);
 	i = 0;
-	while (i < configs->phil_num)
+	while (i < philos->configs->phils_num)
 	{
-		if (pthread_create(&tid, NULL, &routine, NULL) != 0)
-		{
-			return (-1);
-		}
+		pthread_create(&tid[i], NULL, &routine, &philos[i]);
+		i++;
+	}
+	i = 0;
+	while (i < philos->configs->phils_num)
+	{
+		pthread_join(tid[i], NULL);
 		i++;
 	}
 	return (0);
@@ -102,11 +124,10 @@ int	init_threads(t_configs *configs)
 
 int	main(int argc, char **argv)
 {
-	t_configs	*configs;
+	t_philos	*philos;
 
-	
 	if (argc != 5 && argc != 6)
 		return (printf("Number of args is %d: should be 5 or 6\n", argc));
-	configs = init_configs(argc, argv);
-	init_threads(configs);
+	philos = init_philos(argc, argv);
+	init_threads(philos);
 }
