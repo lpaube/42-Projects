47a2bbc9a7160193904ab7fd16dfaa54de8efb34
laube <lpaube@hotmail.com>
2021-08-01 15:01:31 -0300

    priority queue functional

--- philo/srcs/action.c
+++ philo/srcs/action.c
@@ -6,37 +6,89 @@
 /*   By: laube <louis-philippe.aube@hotmail.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 21:55:31 by laube             #+#    #+#             */
-/*   Updated: 2021/08/01 00:50:03 by laube            ###   ########.fr       */
+/*   Updated: 2021/08/01 15:01:18 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incls/philo.h"
 
-void	get_in_queue(t_philos *phil)
+void	get_in_queue(t_philos *phil, t_configs *conf)
 {
 	int	i;
-	int	tmp;
-	int *queue;
 
-	queue = phil->configs->queue;
+	/*
+	printf("getinQueue before: (id: %d) [", phil->id);
+	for (int j = 0; j < conf->phils_num; j++)
+		printf("%d, ", conf->queue[j]);
+	printf("]\n");
+	*/
 	i = 0;
-	while (i < phil->configs->phils_num)
+	while (i < conf->phils_num)
 	{
-		if (phil->id == queue[i++])
+		if (phil->id == conf->queue[i])
+		{
+			/*
+			printf("getinQueue after (id: %d alin | fork lef: %d | fork ri: %d): [", phil->id, conf->forks[phil->id], conf->forks[(phil->id + 1) % conf->phils_num]);
+			for (int j = 0; j < conf->phils_num; j++)
+				printf("%d, ", conf->queue[j]);
+			printf("]\n");
+			*/
 			return ;
+		}
+		// Placing ID at end of queue
+		else if (conf->queue[i] == -1)
+		{
+			conf->queue[i] = phil->id;
+			return ;
+		}
+		i++;
 	}
-	i = phil->configs->phils_num - 1;
-	while (i > 0)
-	{
-		queue[i] = queue[i - 1];
-		i--;
-	}
-	queue[0] = phil->id;
+	/*
+	printf("getinQueue after: (id: %d) [", phil->id);
+	for (int j = 0; j < conf->phils_num; j++)
+		printf("%d, ", conf->queue[j]);
+	printf("]\n");
+//	*/
 }
 
-int	check_queue(t_philos *phil)
+int	check_queue(t_philos *phil, t_configs *conf)
 {
+	int	i;
+	int	id;
 
+	/*
+	printf("checkQueue before: (id: %d) [", phil->id);
+	for (int j = 0; j < conf->phils_num; j++)
+		printf("%d, ", conf->queue[j]);
+	printf("]\n");
+//	*/
+	i = 0;
+	while (i < conf->phils_num && conf->queue[i] != -1)
+	{
+		id = conf->queue[i];
+		if (id == phil->id)
+		{
+			while (i < conf->phils_num - 1)
+			{
+				conf->queue[i] = conf->queue[i + 1];
+				i++;
+			}
+			conf->queue[i] = -1;
+			return (1);
+		}
+		if (conf->forks[id] && conf->forks[(id + 1) % conf->phils_num])
+		{
+			return (0);
+		}
+		i++;
+	}
+	/*
+	printf("checkQueue is not in: (id: %d) [", phil->id);
+	for (int j = 0; j < conf->phils_num; j++)
+		printf("%d, ", conf->queue[j]);
+	printf("]\n");
+//	*/
+	return (1);
 }
 
 void	to_eat(t_configs *conf, t_philos *phil)
@@ -47,19 +99,20 @@ void	to_eat(t_configs *conf, t_philos *phil)
 		pthread_mutex_lock(&conf->f_mutex[(phil->id + 1) % conf->phils_num]);
 		if (conf->forks[phil->id] && conf->forks[(phil->id + 1) % conf->phils_num])
 		{
-			if (check_queue(phil) == 0)
-				return ;
-			conf->forks[phil->id] = 0;
-			printf("%d %d has taken a fork\n", get_time() - conf->start_time, phil->id + 1);
-			conf->forks[(phil->id + 1) % conf->phils_num] = 0;
-			printf("%d %d has taken a fork\n", get_time() - conf->start_time, phil->id + 1);
-			phil->state = 'e';
-			phil->ate_num++;
-			printf("%d %d is eating\n", get_time() - conf->start_time, phil->id + 1);
-			phil->last_meal_time = get_time();
+			if (check_queue(phil, conf) == 1)
+			{
+				conf->forks[phil->id] = 0;
+				printf("%d %d has taken a fork\n", get_time() - conf->start_time, phil->id + 1);
+				conf->forks[(phil->id + 1) % conf->phils_num] = 0;
+				printf("%d %d has taken a fork\n", get_time() - conf->start_time, phil->id + 1);
+				phil->state = 'e';
+				phil->ate_num++;
+				printf("%d %d is eating\n", get_time() - conf->start_time, phil->id + 1);
+				phil->last_meal_time = get_time();
+			}
 		}
 		else
-			get_in_queue(phil);
+			get_in_queue(phil, conf);
 		pthread_mutex_unlock(&conf->f_mutex[phil->id]);
 		pthread_mutex_unlock(&conf->f_mutex[(phil->id + 1) % conf->phils_num]);
 		if (meal_quota(conf))
