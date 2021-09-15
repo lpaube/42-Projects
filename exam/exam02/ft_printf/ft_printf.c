d5d9420580c50cefbf425c9608960e1f7870fda9
LPaube <lpaube@hotmail.com>
2021-09-15 18:34:10 -0400

    working on rank03

--- exam/exam02/ft_printf/ft_printf.c
+++ exam/exam02/ft_printf/ft_printf.c
@@ -1,16 +1,6 @@
-#include <stdarg.h>
 #include <unistd.h>
-#include <stdlib.h>
-
+#include <stdarg.h>
 #include <stdio.h>
-
-typedef struct s_flags
-{
-	char	type;
-	int		width;
-	int		precision;
-	int		precision_active;
-}	t_flags;
 
 int	ft_strlen(char *str)
 {
@@ -22,225 +12,150 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-void	ft_putstr(char *str)
+int	ft_numlen(long long num, int base_len)
 {
-	write(1, str, ft_strlen(str));
+	unsigned int len = 1;
+
+	while (num >= base_len)
+	{
+		len++;
+		num /= base_len;
+	}
+	return (len);
 }
 
-void	ft_putchar(char c)
+void	ft_putnbr(long long num, int base_len, char *base)
 {
+	char	c;
+
+	if (num >= base_len)
+		ft_putnbr(num / base_len, base_len, base);
+	c = base[(num % base_len)];
+	//printf("char: %c\n", c);
 	write(1, &c, 1);
-}
-
-int	ft_atoi(const char **fmt)
-{
-	 int	num;
-
-	 num = 0;
-	 while (**fmt >= '0' && **fmt <= '9')
-	 {
-		num = (num * 10) + (**fmt - '0');
-		(*fmt)++;
-	 }
-	 return (num);
-}
-
-t_flags	set_flags(const char **fmt)
-{
-	t_flags	flag;
-
-	(*fmt)++;
-	flag.width = ft_atoi(fmt);
-	if (**fmt == '.')
-	{
-		flag.precision_active = 1;
-		(*fmt)++;
-		flag.precision = ft_atoi(fmt);
-	}
-	else
-	{
-		flag.precision_active = 0;
-		flag.precision = 0;
-	}
-	flag.type = **fmt;
-	(*fmt)++;
-	return (flag);
-}
-
-void	convert_s(t_flags flag, char *str, int *counter)
-{
-	int	printed_chars;
-	int	padding;
-
-	if (!str)
-		str = "(null)";
-	padding = 0;
-	printed_chars = ft_strlen(str);
-	if (flag.precision < printed_chars && flag.precision_active)
-		printed_chars = flag.precision;
-	if (flag.width > printed_chars)
-		padding = flag.width - printed_chars;
-	while (padding)
-	{
-		ft_putchar(' ');
-		(*counter)++;
-		padding--;
-	}
-	while (printed_chars)
-	{
-		ft_putchar(*str);
-		(*counter)++;
-		str++;
-		printed_chars--;
-	}
-}
-
-void	print_hex(unsigned int num, int *counter)
-{
-
-	int	num_res = num;
-	if (num > 15)
-		print_hex(num / 16, counter);
-	num_res = num % 16;
-	if (num_res < 0)
-		num_res = -num_res;
-	ft_putchar("0123456789abcdef"[num_res]);
-	(*counter)++;
-}
-
-void	convert_x(t_flags flag, unsigned int num, int *counter)
-{
-	int	nbr_length = 1;
-	int	width_padding = 0;
-	int	prec_padding = 0;
-	unsigned int	tmp = num;
-	int	neg = 0;
-
-	if (num < 0)
-		neg = 1;
-	while (tmp > 15)
-	{
-		nbr_length++;
-		tmp /= 16;
-	}
-	if (flag.precision_active)
-		prec_padding = flag.precision - nbr_length;
-	width_padding = flag.width - prec_padding - nbr_length - neg;
-	while (width_padding > 0)
-	{
-		ft_putchar(' ');
-		(*counter)++;
-		width_padding--;
-	}
-	if (num < 0)
-	{
-		ft_putchar('-');
-		(*counter)++;
-	}
-	while (prec_padding > 0)
-	{
-		ft_putchar('0');
-		(*counter)++;
-		prec_padding--;
-	}
-	print_hex(num, counter);
-}
-
-int	get_printed_chars(int num)
-{
-	int	printed_chars = 0;
-
-	if (num == -2147483648)
-		return (10);
-	if (num == 0)
-		return (1);
-	while (num)
-	{
-		printed_chars++;
-		num = num / 10;
-	}
-	return (printed_chars);
-}
-
-void	ft_putnbr(int num)
-{
-	if (num == -2147483648)
-	{
-		ft_putstr("-2147483648");
-		return ;
-	}
-	else if (num < 0)
-	{
-		num = -num;
-	}
-	if (num >= 10)
-		ft_putnbr(num / 10);
-	ft_putchar((num % 10) + '0');
-}
-
-void	convert_d(t_flags flag, int num, int *counter)
-{
-	int	printed_chars;
-	int	width_padding;
-	int	prec_padding = 0;
-
-	printed_chars = get_printed_chars(num);
-	if (flag.precision_active && flag.precision > printed_chars)
-		prec_padding = flag.precision - printed_chars;
-	if (num < 0)
-		printed_chars++;
-	width_padding = flag.width - printed_chars - prec_padding;
-	while (width_padding > 0)
-	{
-		ft_putchar(' ');
-		(*counter)++;
-		width_padding--;
-	}
-	if (num < 0)
-	{
-		ft_putchar('-');
-	}
-	while (prec_padding > 0)
-	{
-		ft_putchar('0');
-		(*counter)++;
-		prec_padding--;
-	}
-	ft_putnbr(num);
-	*counter += printed_chars;
-}
-
-void	convert_it(t_flags flag, va_list *ap, int *counter)
-{
-	if (flag.type == 's')
-		convert_s(flag, va_arg(*ap, char*), counter);
-	if (flag.type == 'd')
-		convert_d(flag, va_arg(*ap, int), counter);
-	if (flag.type == 'x')
-		convert_x(flag, va_arg(*ap, int), counter);
 }
 
 int	ft_printf(const char *fmt, ... )
 {
-	va_list	ap;
-	t_flags	flag;
-	int	counter;
+	int prec = 0, zeros = 0, spaces = 0, width = 0, prec_len = 0,
+		len = 0, base_len = 0, negative = 0, ret = 0;
+	va_list	list;
+	char	*str;
+	char	*base;
+	long long num = 0;
+	void	*addr;
 
-	counter = 0;
-	va_start(ap, fmt);
+	va_start(list, fmt);
 	while (*fmt)
 	{
 		if (*fmt == '%')
 		{
-			flag = set_flags(&fmt);
-			convert_it(flag, &ap, &counter);
+			fmt++;
+			zeros = 0, spaces = 0, width = 0, prec_len = 0, len = 0, base_len = 0,
+				  negative = 0;
+			while (*fmt >= '0' && *fmt <= '9')
+			{
+				width = width * 10 + (*fmt - '0');
+				fmt++;
+			}
+			if (*fmt == '.')
+			{
+				prec = 1;
+				fmt++;
+				while (*fmt >= '0' && *fmt <= '9')
+				{
+					prec_len = prec_len * 10 + (*fmt - '0');
+					fmt++;
+				}
+			}
+			if (*fmt == 's')
+			{
+				str = va_arg(list, char *);
+				if (!str)
+					str = "(null)";
+				len = ft_strlen(str);
+			}
+			if (*fmt == 'd')
+			{
+				base_len = 10;
+				base = "0123456789";
+				num = va_arg(list, int);
+				if (num < 0)
+				{
+					negative = 1;
+					num = -num;
+				}
+				len = ft_numlen(num, base_len);
+			}
+			if (*fmt == 'x')
+			{
+				base_len = 16;
+				base = "0123456789abcdef";
+				num = va_arg(list, unsigned int);
+				len = ft_numlen(num, base_len);
+			}
+			if (*fmt == 'p')
+			{
+				base_len = 16;
+				base = "0123456789abcdef";
+				num = (unsigned long long)va_arg(list, void *);
+				len = ft_numlen(num, base_len) + 2;
+			}
+			if (prec)
+			{
+				if (prec_len > len && *fmt != 's')
+					zeros = prec_len - len;
+				if (prec_len < len && *fmt == 's')
+					len = prec_len;
+				if (!prec_len && !num)
+					len = 0;
+			}
+			spaces = width - zeros - len - negative;
+
+			while (spaces-- > 0)
+			{
+				write(1, " ", 1);
+				ret++;
+			}
+			if (negative)
+			{
+				write(1, "-", 1);
+				ret++;
+			}
+			while (zeros-- > 0)
+			{
+				write(1, "0", 1);
+				ret++;
+			}
+			if (*fmt == 'p')
+				write(1, "0x", 2);
+			if (*fmt == 's')
+				write(1, str, len);
+			else if (len)
+			{
+				//printf("num: %lld\n", num);
+				ft_putnbr(num, base_len, base);
+			}
+			ret += len;
 		}
 		else
 		{
-			ft_putchar(*fmt);
-			fmt++;
-			counter++;
+			write(1, &*fmt, 1);
+			ret++;
 		}
+		fmt++;
 	}
-	return (counter);
+	va_end(list);
+	return (ret);
+}
+
+int	main(void)
+{
+	int	ret;
+	char *sss = "ok lol";
+	ret = printf("REAL: num: %d str: %s ptr: %p hex: %x\n", -15, "whatf", sss, 36);
+	printf("REAL: ret: %d\n", ret);
+
+	ret = ft_printf("MINE: num: %d str: %s ptr: %p hex: %x\n", -15, "whatf", sss, 36);
+	printf("MINE: ret: %d\n", ret);
 }
---
+++ exam/exam02/get_next_line/yours_.res
@@ -0,0 +1,21 @@
+#include "get_next_line.h"
+
+int	get_next_line(char **line)
+{
+	int	i = 0;
+	int	r;
+	char c;
+	char *buffer;
+
+	if (!(buffer = malloc(10000)))
+		return (-1);
+	while ((r = read(0, &c, 1)) && c != '\n' && c != '\0')
+	{
+		if (c != '\n' && c != '\0')
+			buffer[i] = c;
+		i++;
+	}
+	buffer[i] = '\0';
+	*line = buffer;
+	return (r);
+}
